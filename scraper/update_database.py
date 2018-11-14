98b256e43a305a4276e7e9ad06b25338d9018e6b
Arthur Van Remoortel <arthurvanremoortel@gmail.com>
2018-11-14 09:02:41 +0100

    Updated db

--- scraper/update_database.py
+++ scraper/update_database.py
@@ -1,12 +1,17 @@
 import sqlite3
 import os
-import pprint
+from pprint import pprint
 from time import sleep
 import array
 from ab_parser import ABScraper
 from flagey2 import FlageyScraper
 import json
-SCRAPERS = [ABScraper, FlageyScraper]
+import sys
+
+
+SCRAPERS = [ABScraper,
+            FlageyScraper
+            ]
 
 
 def get_event(conn, remote_id, title):
@@ -17,18 +22,31 @@ def get_event(conn, remote_id, title):
 def add_new_event(conn, event_title, event_url, remote_id, event_image, event_datetime, event_description, event_price, event_tags, previews):
     command1 = "insert into events (title, remote_id, event_datetime, event_price, event_image_url, event_description, event_url) \
                 VALUES (?, ?, ?, ?, ?, ?, ?);"
-    conn.execute(command1, (event_title, str(remote_id), event_datetime, event_price, event_image, event_description, event_url))
-    conn.commit()
+    cursor = conn.execute(command1, (event_title, str(remote_id), event_datetime, event_price, event_image, event_description, event_url))
+    event_id = cursor.lastrowid
+    for tag in event_tags:
+        add_tag_command = "insert or ignore into tags (tag_name) values (?)"
+        conn.execute(add_tag_command, (tag, ))
+        tag_id = conn.execute("select tag_id from tags where tag_name == ?", (tag, )).fetchone()[0]
+        add_junction_cmd = "insert into event_tags_junction (event_id, tag_id) values (?, ?);"
+        conn.execute(add_junction_cmd, (event_id, tag_id))
 
+    for preview_url in previews:
+        add_preview_command = "insert or ignore into previews (url) values (?)"
+        conn.execute(add_preview_command, (preview_url, ))
+        preview_url_id = conn.execute("select preview_id from previews where url == ?", (preview_url, )).fetchone()[0]
+        add_junction_cmd = "insert into event_previews_junction (event_id, preview_id) values (?, ?);"
+        conn.execute(add_junction_cmd, (event_id, preview_url_id))
+
+    conn.commit()
 
 class DatabaseUpdater:
     def __init__(self):
         self.update_inteval = 60  # In seconds.
         self.should_update = True
         self.conn = sqlite3.connect('database.db')
-        self.start_update_cycle()
 
-    def start_update_cycle(self):
+    def start_update_cycle(self, once=False):
         while self.should_update:
             for Scraper in SCRAPERS:
                 s = Scraper()
@@ -55,10 +73,22 @@ class DatabaseUpdater:
                                       event_dict['event_tags'],
                                       event_dict['previews'])
 
+            if once:
+                self.should_update = False
+                break
             sleep(self.update_inteval)
 
 
 if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        raise Exception('Invalid command line args')
+
     dbu = DatabaseUpdater()
-    dbu.start_update_cycle()
+    run_mode = sys.argv[1]
+    if run_mode == "run-once":
+        print('Only scraping 1 time')
+        dbu.start_update_cycle(once=True)
+    elif run_mode == 'run-forever':
+        dbu.start_update_cycle()
+
 
