fcc5db32b6e6d7df7953d009b654397c6a7603fe
Arthur Van Remoortel <arthurvanremoortel@gmail.com>
2018-11-23 12:12:21 +0100

    Added stuff

--- website_arthur/django1/test1/urls.py
+++ website_arthur/django1/test1/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('venues', views.venues, name='venues'),
+    path('add_venue_form_test', views.add_venue_form_test, name='add_venue_form_test'),
+    path('add_event_form_test', views.add_event_form_test, name='add_event_form_test'),
 ]
--- website_arthur/django1/test1/views.py
+++ website_arthur/django1/test1/views.py
@@ -5,6 +5,21 @@ from django.http import HttpResponse
 
 def index(request):
     context = {
-
+        'search_results': [[1, 2], [3, 4], [5, 6], [7]]
     }
     return render(request, 'index.html', context)
+
+
+def venues(request):
+    context = {}
+    return render(request, 'venues.html', context)
+
+
+def add_venue_form_test(request):
+    context = {}
+    return render(request, 'add_venue_form.html', context)
+
+
+def add_event_form_test(request):
+    context = {}
+    return render(request, 'add_event_form.html', context)
