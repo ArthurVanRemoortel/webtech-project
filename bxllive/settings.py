88e6df5a2ca11f789475c369887c35ea99f40286
lvsz <levisiuzdak@gmail.com>
2018-12-16 15:13:41 +0100

    Configured database for Heroku deployment

--- bxllive/settings.py
+++ bxllive/settings.py
@@ -80,13 +80,17 @@ WSGI_APPLICATION = 'bxllive.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'database',
-        'USER': 'webtech',
-    },
-}
+import dj_database_url
+
+DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#        'NAME': 'database',
+#        'USER': 'webtech',
+#    },
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
