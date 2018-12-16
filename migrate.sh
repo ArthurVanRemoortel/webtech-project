50603951553c4ca1bfe1aa8260ef5e02213dde87
lvsz <levisiuzdak@gmail.com>
2018-12-16 19:15:41 +0100

    Added testdata which is loaded by migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -3,5 +3,6 @@
 perl -i -pe 's/(^.*choices=\[\(o.id, str\(o.name\)\) for o in Venue.objects.all\(\)\])/#$1/' webtech/forms.py
 python3 manage.py makemigrations
 python3 manage.py migrate
+python3 manage.py loaddata testdata.json
 perl -i -pe 's/^#(.*choices=\[\(o.id, str\(o.name\)\) for o in Venue.objects.all\(\)\])/$1/' webtech/forms.py
 
