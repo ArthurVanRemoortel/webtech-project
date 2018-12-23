7c2e73939a52c1f4c43e0febb3c824d80ae9272b
Arthur Van Remoortel <arthurvanremoortel@gmail.com>
2018-12-23 13:28:44 +0100

    Fixed some navab links

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,12 +9,12 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='../login'),name="logout"),
     path('register/', views.register, name='register'),
 
-    path('password_reset/', 		auth_views.PasswordResetView.as_view(template_name='password_reset_form.html') ,name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html') ,name='password_reset_done'),
-    path('password_reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html') ,name='password_reset_confirm'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('password_reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('password_reset/complete', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 
-   	path('password_change/', 	auth_views.PasswordChangeView.as_view(template_name='password_change.html') ,name='password_change'),
-   	path('password_change/done/',auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html') ,name='password_change_done'),
+   	path('password_change/', 	auth_views.PasswordChangeView.as_view(template_name='password_change.html'), name='password_change'),
+   	path('password_change/done/',auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
    	# path('', include('django.contrib.auth.urls')),
 ]
